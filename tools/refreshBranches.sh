aaeb9a7f7a03184626704002d29e4349c745db4a
ybznek <zbynek@ithvezda.cz>
2021-05-03 20:43:00 +0200

    refreshBranches update

--- tools/refreshBranches.sh
+++ tools/refreshBranches.sh
@@ -11,17 +11,17 @@ git fetch marlin bugfix-2.0.x
 
 # merge changes
 git checkout bugfix-2.0.x
-git merge marlin/bugfix-2.0.x
+git merge marlin/bugfix-2.0.x --no-edit
 
 # apply to multiple branches
 git checkout bugfix-2.0.x-cnc
-git merge bugfix-2.0.x
+git merge bugfix-2.0.x --no-edit
 
 git checkout bugfix-2.0.x-endy
-git merge bugfix-2.0.x
+git merge bugfix-2.0.x --no-edit
 
 git checkout bugfix-2.0.x-endy-junior
-git merge bugfix-2.0.x
+git merge bugfix-2.0.x --no-edit
 
 
 # return to base branch
