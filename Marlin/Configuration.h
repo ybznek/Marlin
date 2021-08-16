08098fbca1072543c0f8f21bcb39c8795c86b230
ybznek <zbynek@ithvezda.cz>
2021-08-17 00:13:35 +0200

    pid

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -602,9 +602,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp  35.55
+    #define DEFAULT_Ki   4.02
+    #define DEFAULT_Kd 78.57
   #endif
 #endif // PIDTEMP
 
